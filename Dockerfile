c07cc6162a130993fceae59adc84a5181aa0632d
Vincent Pham <vpham@nevada.unr.edu>
2021-11-29 06:22:13 -0800

    fix(_app.js-index.js): add mui library

--- Dockerfile
+++ Dockerfile
@@ -4,25 +4,29 @@ LABEL version="1.0"
 LABEL description="Dev Docker Image for Dyad's web frontend"
 LABEL maintainer = ["vpham@nevada.unr.edu"]
 
+RUN mkdir -p /usr/app
+
 WORKDIR /usr/app
+
+ENV PORT 3000
 
 # PM2 for NodeJS Daemon
 RUN npm install --global pm2
 
 # Copy package.json and package-lock.json
-COPY ./package*.json ./
+COPY dyad-web/package*.json /usr/app
 
 # Install
 RUN npm install
 
-# Copy rest of files
-COPY ./ ./
+# Copy source files
+COPY dyad-web/ /usr/app
 
 # Build app
 RUN npm run build
 
 # Expose port
-EXPOSE 1337
+EXPOSE 3000
 
 # Run container as non-root user included with Alpine base img
 USER node
--- dyad-web/pages/_app.js
+++ dyad-web/pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+  pallete: {
+    primary: {
+      main: '#8FBCBB',
+    },
+    secondary: {
+      main: '#8FBCBB',
+    },
+    error: {
+      main: '#8FBCBB',
+    },
+  },
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Dyad</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  ); 
 }
-
-export default MyApp
