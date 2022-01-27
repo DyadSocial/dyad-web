358c554242eba7f632df8eec6bf02219ccdaa148
Vincent Pham <vpham@nevada.unr.edu>
2022-01-26 21:52:23 -0800

    (pages/..., styles/...) styled entire webpage, add global color and font variables, fix styling issues, made compatible with default linter settings

--- dyad-web/pages/index.js
+++ dyad-web/pages/index.js
@@ -1,33 +1,31 @@
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import Link from '@mui/material/Link';
-import NextLink from 'next/link';
-
-function SeeAbout() {
-  return (
-    <Typography sx={{mt: 6, mb:3}} color="text.secondary">
-      Welcome to the home page for Dyad - A local social network. To see more info about this application, click{' '}
-      <NextLink href="/about"> 
-        <Link href="/about">here</Link>
-      </NextLink>.
-    </Typography>
-  )
-}
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import * as React from "react";
+import Link from "@mui/material/Link";
+import NextLink from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Container maxWidth="sm">
-      <center>
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Dyad - A local social network.
-        </Typography>
-      </Box>
-      </center>
-      <SeeAbout />
-    </Container>
-  )
+    <div className={styles["page-root"]}>
+      <Container maxWidth="sm">
+        <center>
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Dyad - A local social network.
+            </Typography>
+          </Box>
+        </center>
+        <p>
+          Welcome to the home page for Dyad - A local social network. To see
+          more info about this application, click{" "}
+          <NextLink href="/about">
+            <Link href="/about">here</Link>
+          </NextLink>
+          .
+        </p>
+      </Container>
+    </div>
+  );
 }
