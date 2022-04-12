ae77fbbdc9ef112807128b3da372439695cba02f
Jake D <jakob.delossantos1@gmail.com>
2022-04-11 22:15:52 -0700

    Added a timeline to the about page for instructors to see progress

--- dyad-web/pages/about.js
+++ dyad-web/pages/about.js
@@ -2,6 +2,61 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import * as React from "react";
 import styles from "../styles/About.module.css";
+import { Timeline, TimelineItem, TimelineConnector, TimelineContent, TimelineDot, TimelineSeparator, TimelineOppositeContent } from "@mui/lab";
+import Typography from '@mui/material/Typography';
+import ArrowCircleDownOutlined from '@mui/icons-material/ArrowCircleDownOutlined';
+
+let events = [
+  //Event 1
+  {
+    title: 'Project Design',
+    event: 'The initial design for Dyad was pitched to instructors and accepted. Dyad will use Bluetooth P2P to connect users as a new type of social media.',
+    date: 'October 2021',
+  },
+  //Event 2
+  {
+    title: 'Project Prototype',
+    event: 'The first prototype of Dyad was completed. The prototype had a login screen with working authentication. It brought the users to the feed page.',
+    date: 'November 2021',
+  },
+  //Event 3
+  {
+    title: 'First Project Demo',
+    event: 'The prototype was demo\'d by the team. It was advised that the team implement more features, but it was looking good so far.',
+    date: 'December 2021',
+  },
+  //Event 4
+  {
+    title: 'Finishing the UI',
+    event: 'The UI of the application was completed. The application is presented in a Nord theme, and had a UI for the feed, messaging, and user profiles.',
+    date: 'February 2022',
+  },
+  //Event 5
+  {
+    title: 'Change of plans',
+    event: 'After the mid-semester demo, the team decided to switch from Bluetooth P2P connection to city-based networking using geolocation and geo-IP.',
+    date: 'March 2022',
+  },
+  //Event 6
+  {
+    title: 'Networking',
+    event: 'The networking was the most difficult aspect of the project, but was completed using geolocation and geo-IP. Users on the application will now create & see posts from within their own city.',
+    date: 'March-April 2022',
+  },
+  //Event 7
+  {
+    title: 'Finishing touches',
+    event: 'The team went through fixing various bugs, such as displaying and transferring images using gRPC. More features were added to the application, such as a map to show users where posts come from, and comments on posts. A desktop application also was prototyped.',
+    date: 'April 2022',
+  },
+  //Event 8
+  {
+    title: 'Innevation Day',
+    event: 'The team presented their project at UNR\'s Innevation Day, with the 25+ projects of their peers.',
+    date: 'May 2022',
+  },
+
+]
 
 export default function About() {
   return (
@@ -104,10 +159,186 @@ export default function About() {
                 </ul>
               </p>
             </div>
+            <div className={styles["description-title"]}>
+              <h1>Project Progress</h1>
+              <Timeline position="alternate">
+                {/* Event 1 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[0].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[0].date}
+                    </Typography>
+                    <Typography>
+                      {events[0].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem> 
+                {/* Event 2 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[1].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[1].date}
+                    </Typography>
+                    <Typography>
+                      {events[1].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                {/* Event 3 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[2].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[2].date}
+                    </Typography>
+                    <Typography>
+                      {events[2].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                {/* Event 4 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[3].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[3].date}
+                    </Typography>
+                    <Typography>
+                      {events[3].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                {/* Event 5 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[4].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[4].date}
+                    </Typography>
+                    <Typography>
+                      {events[4].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                {/* Event 6 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[5].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[5].date}
+                    </Typography>
+                    <Typography>
+                      {events[5].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                {/* Event 7 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[6].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[6].date}
+                    </Typography>
+                    <Typography>
+                      {events[6].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                {/* Event 8 */}
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot color="primary" variant="outlined">
+                      <ArrowCircleDownOutlined />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Typography variant="h5" component="span">
+                      {events[7].title}
+                      <br/>
+                    </Typography>
+                    <Typography variant="h9" component="span" color="text.secondary">
+                      {events[7].date}
+                    </Typography>
+                    <Typography>
+                      {events[7].event}
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+              </Timeline>
+            </div>
+            <div className={styles["description-title"]}>
+              <h1>Project Poster & Video</h1>
+            </div>
           </div>
         </div>
       </center>
     </div>
-    //TODO: Project related resources part of Canvas page
+    
   );
 }
